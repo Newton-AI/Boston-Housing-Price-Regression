97e0b59e9b053c136b94ff7f463ab6e15e23ab98
Louis JZ <louis.ljz08@gmail.com>
2023-03-25 23:08:41 +0700

    add nox feature

--- webapp.py
+++ webapp.py
@@ -13,7 +13,7 @@ model = joblib.load(os.path.join(folder_path, 'model.pkl'))
 ss = StandardScaler()
 
 def predict():
-    features = np.array([[rm, lstat, dis, crim]])
+    features = np.array([[rm, lstat, dis, crim, nox]])
     scaled_features = ss.fit_transform(features.T)
 
     pred = model.predict(scaled_features.T)
@@ -26,6 +26,7 @@ with tab1:
     lstat = st.number_input('Lower status of the population (percent)', min_value=0.0, max_value=100.0, step=0.1)
     dis = st.number_input('Weighted mean of distances to five Boston employment centres', step=0.1)
     crim = st.number_input('Per capita crime rate by town', step=0.1)
+    nox = st.number_input('nitrogen oxides concentration (parts per 10 million).', min_value=0.0, max_value=1.0, step=0.01)
 
     st.button('Predict', on_click=predict)
 
@@ -33,8 +34,8 @@ with tab2:
     st.markdown('This model is trained using **:blue[Random Forest Regressor]** Model.')
 
     col1, col2, col3 = st.columns(3)
-    col1.metric(label='R2 Score', value='82%')
-    col2.metric(label='RMSE', value=3.43)
-    col3.metric(label='MAE', value=2.46)
+    col1.metric(label='R2 Score', value='86%')
+    col2.metric(label='RMSE', value=3.05)
+    col3.metric(label='MAE', value=2.3)
 
     st.image(os.path.join(folder_path, 'model report.png'), caption='Compare labels and predictions for testing data')
